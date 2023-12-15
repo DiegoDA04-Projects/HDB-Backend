aeb8af87ea8389312ab615555182a9ff5ba6af7b
diegoda04 <u20201c334@upc.edu.pe>
2023-12-15 14:37:04 -0500

    chore: initial commit

---
+++ src/main/java/com/nerv/hdbmanagement/HdbBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nerv.hdbmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdbBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HdbBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nerv/hdbmanagement/HdbBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nerv.hdbmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HdbBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
