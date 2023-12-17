b074b929cea32d6ec57ef246ea56d8b6b6c1a99f
diegoda04 <u20201c334@upc.edu.pe>
2023-12-16 23:33:05 -0500

    feat(controller): added Properties controller for CRUD operations
    - Create PropertiesController to handle API requests related to properties
    - Utilize PropertyService for business logic and PropertyMapper for model-resource mapping
    - Implement endpoints for getting all properties, getting a property by ID, creating a property, updating a property, and deleting a property
    - Return ResponseEntity with appropriate HTTP status codes and mapped resources

---
+++ src/main/java/com/nerv/hdbmanagement/properties/interfaces/rest/PropertiesController.java
@@ -0,0 +1,49 @@
+package com.nerv.hdbmanagement.properties.interfaces.rest;
+
+import com.nerv.hdbmanagement.properties.domain.service.PropertyService;
+import com.nerv.hdbmanagement.properties.interfaces.rest.mapping.PropertyMapper;
+import com.nerv.hdbmanagement.properties.interfaces.rest.resources.CreatePropertyResource;
+import com.nerv.hdbmanagement.properties.interfaces.rest.resources.PropertyResource;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/properties")
+public class PropertiesController {
+
+    private final PropertyService propertyService;
+    private final PropertyMapper mapper;
+
+    public PropertiesController(PropertyService propertyService, PropertyMapper mapper) {
+        this.propertyService = propertyService;
+        this.mapper = mapper;
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<PropertyResource>> getAllProperties(Pageable pageable) {
+        return new ResponseEntity<>(mapper.modelListPage(propertyService.getAll(), pageable), HttpStatus.OK);
+    }
+
+    @GetMapping("{propertyId}")
+    public ResponseEntity<PropertyResource> getPropertyById(@PathVariable Long propertyId) {
+        return new ResponseEntity<>(mapper.toResource(propertyService.getById(propertyId)), HttpStatus.OK);
+    }
+
+    @PostMapping()
+    public ResponseEntity<PropertyResource> createProperty(@RequestBody CreatePropertyResource resource) {
+        return new ResponseEntity<>(mapper.toResource(propertyService.create(mapper.toModel(resource))), HttpStatus.CREATED);
+    }
+
+    @PutMapping("{propertyId}")
+    public ResponseEntity<PropertyResource> updateProperty(@PathVariable Long propertyId, @RequestBody CreatePropertyResource resource) {
+        return new ResponseEntity<>(mapper.toResource(propertyService.update(propertyId, mapper.toModel(resource))), HttpStatus.OK);
+    }
+
+    @DeleteMapping("{propertyId}")
+    public ResponseEntity<?> deleteProperty(@PathVariable Long propertyId) {
+        return propertyService.delete(propertyId);
+    }
+}
