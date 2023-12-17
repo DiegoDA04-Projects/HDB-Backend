3a09359df28805ba047f1f2541e35f043c1796ef
diegoda04 <u20201c334@upc.edu.pe>
2023-12-17 00:01:47 -0500

    chore(application): enabled JPA Auditing

--- src/main/java/com/nerv/hdbmanagement/HdbBackendApplication.java
+++ src/main/java/com/nerv/hdbmanagement/HdbBackendApplication.java
@@ -2,8 +2,10 @@ package com.nerv.hdbmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class HdbBackendApplication {
 
     public static void main(String[] args) {
