27cff3f5463359c7858de4f22bb501946471a919
diegoda04 <u20201c334@upc.edu.pe>
2023-12-17 02:17:38 -0500

    docs(controller): added OpenAPI documentation to PropertiesController endpoints
    - Include OpenAPI documentation annotations (Operation, Tag) in PropertiesController
    - Summarize each endpoint's purpose and add description for the Properties tag

--- src/main/java/com/nerv/hdbmanagement/properties/interfaces/rest/PropertiesController.java
+++ src/main/java/com/nerv/hdbmanagement/properties/interfaces/rest/PropertiesController.java
@@ -4,14 +4,18 @@ import com.nerv.hdbmanagement.properties.domain.service.PropertyService;
 import com.nerv.hdbmanagement.properties.interfaces.rest.mapping.PropertyMapper;
 import com.nerv.hdbmanagement.properties.interfaces.rest.resources.CreatePropertyResource;
 import com.nerv.hdbmanagement.properties.interfaces.rest.resources.PropertyResource;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/properties")
+@RequestMapping(value = "/api/v1/properties", produces = MediaType.APPLICATION_JSON_VALUE)
+@Tag(name = "Properties", description = "Properties Management endpoints")
 public class PropertiesController {
 
     private final PropertyService propertyService;
@@ -23,6 +27,7 @@ public class PropertiesController {
     }
 
     @GetMapping
+    @Operation(summary = "Get all properties")
     public ResponseEntity<Page<PropertyResource>> getAllProperties(@RequestParam(defaultValue = "0") int page,
                                                                    @RequestParam(defaultValue = "10") int size) {
 
@@ -30,21 +35,25 @@ public class PropertiesController {
     }
 
     @GetMapping("{propertyId}")
+    @Operation(summary = "Get property by id")
     public ResponseEntity<PropertyResource> getPropertyById(@PathVariable Long propertyId) {
         return new ResponseEntity<>(mapper.toResource(propertyService.getById(propertyId)), HttpStatus.OK);
     }
 
     @PostMapping()
+    @Operation(summary = "Create property")
     public ResponseEntity<PropertyResource> createProperty(@RequestBody CreatePropertyResource resource) {
         return new ResponseEntity<>(mapper.toResource(propertyService.create(mapper.toModel(resource))), HttpStatus.CREATED);
     }
 
     @PutMapping("{propertyId}")
+    @Operation(summary = "Update property")
     public ResponseEntity<PropertyResource> updateProperty(@PathVariable Long propertyId, @RequestBody CreatePropertyResource resource) {
         return new ResponseEntity<>(mapper.toResource(propertyService.update(propertyId, mapper.toModel(resource))), HttpStatus.OK);
     }
 
     @DeleteMapping("{propertyId}")
+    @Operation(summary = "Delete property")
     public ResponseEntity<?> deleteProperty(@PathVariable Long propertyId) {
         return propertyService.delete(propertyId);
     }
